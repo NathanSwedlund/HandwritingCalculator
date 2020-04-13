a0f55b94ea56d5b30ec7cc5a2791057d23055429
brighton-mica <bdm98a@gmail.com>
2020-04-13 08:45:39 -0500

    Fixed onMouseLeave bug and offset bug in HTML canvas

--- HandwritingApp/static/js/script.js
+++ HandwritingApp/static/js/script.js
@@ -4,80 +4,84 @@ var lastY = 0;
 
 // Setting up canvas
 var canvas = document.querySelector("canvas");
-updateCanvasSize()
+updateCanvasSize();
 var ctx = canvas.getContext("2d");
 // ---
 
 // Generic Functions ---------------------------------
-function updateCanvasSize()
-{
-    canvas.width = window.innerWidth * 0.9;
-    canvas.height = window.innerHeight * 0.2;
+function updateCanvasSize() {
+  canvas.width = window.innerWidth * 0.9;
+  canvas.height = window.innerHeight * 0.2;
 }
 
 function mousePosition(canvas, evt) {
   var box = canvas.getBoundingClientRect();
-  var X = evt.clientX - box.left;
-  var Y = evt.clientY - box.top;
-  return {x:X, y:Y};
+  //   var X = evt.clientX - box.left;
+  //   var Y = evt.clientY - box.top;
+  return { x: evt.offsetX, y: evt.offsetY };
 }
 
-window.addEventListener("resize", updateCanvasSize);
+function mouseLeave(e) {
+  mouseIsDown = false;
+}
 
 // Setting event listeners ------------------------------
-canvas.addEventListener("mousemove", function (evt) {
-    if(lastX == 0 || lastY == 0 || !mouseIsDown)
-    {
-        lastX = x;
-        lastY = y;
-        return;
-    }
+window.addEventListener("resize", updateCanvasSize);
+canvas.addEventListener("mouseleave", mouseLeave);
 
-    console.log("drawing");
-    var pos = mousePosition(canvas, evt);
-    var x = pos.x
-    var y = pos.y
-
-    // Strokes from the previous mouse location to the current one
-    ctx.save();
-    ctx.beginPath();
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = 3;
-    ctx.moveTo(lastX, lastY);
-    ctx.lineTo(x, y);
-    ctx.stroke();
-    ctx.restore();
-
-    // Updating last known mouse location
+canvas.addEventListener("mousemove", (evt) => {
+  if (lastX == 0 || lastY == 0 || !mouseIsDown) {
     lastX = x;
     lastY = y;
+    return;
+  }
+
+  console.log("drawing");
+  //   var pos = mousePosition(canvas, evt);
+  var x = evt.offsetX;
+  var y = evt.offsetY;
+
+  // Strokes from the previous mouse location to the current one
+  ctx.save();
+  ctx.beginPath();
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 3;
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(x, y);
+  ctx.stroke();
+  ctx.restore();
+
+  // Updating last known mouse location
+  lastX = x;
+  lastY = y;
 });
 
 var mouseIsDown = false;
-canvas.addEventListener("mousedown", function (evt) {
-    mouseIsDown = true;
-    var pos = mousePosition(canvas, evt);
-    lastX = pos.x;
-    lastY = pos.y;
+canvas.addEventListener("mousedown", (evt) => {
+  mouseIsDown = true;
+  //   var pos = mousePosition(canvas, evt);
+  lastX = evt.offsetX;
+  lastY = evt.offsetY;
 });
-canvas.addEventListener("mouseup",   function (evt) {
-    mouseIsDown = false;
-    lastX = 0;
-    lastY = 0;
+canvas.addEventListener("mouseup", function (evt) {
+  mouseIsDown = false;
+  lastX = 0;
+  lastY = 0;
 });
 
-document.getElementById("clearButton").onclick = function ()
-{
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
+document.getElementById("clearButton").onclick = function () {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+};
 
 // Sends the canvas as an image to python
-document.getElementById("exportButton").onclick = function ()
-{
-    var img = canvas.toDataURL("image/png");
-    // Sending image to python
-    $.post("/evaluate", {testVal: img}, function(response){
-        if(response === 'success'){ console.log("POST image success!  ln:84, file:script.js"); }
-        else{ alert("Error POST-ing: ln:84, file:script.js"); }
-    });
-}
+document.getElementById("exportButton").onclick = function () {
+  var img = canvas.toDataURL("image/png");
+  // Sending image to python
+  $.post("/evaluate", { testVal: img }, function (response) {
+    if (response === "success") {
+      console.log("POST image success!  ln:84, file:script.js");
+    } else {
+      alert("Error POST-ing: ln:84, file:script.js");
+    }
+  });
+};
